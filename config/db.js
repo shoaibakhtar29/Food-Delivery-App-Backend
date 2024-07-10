3471d820d47760dfef2404e74c1472a896adf899
Shoaib Akhtar <shoaibakhtar2910@gmail.com>
2024-07-11 01:10:46 +0530

    reinitialize

--- config/db.js
+++ config/db.js
@@ -1,7 +1,8 @@
 import mongoose from "mongoose";
-
+import "dotenv/config"
+const db_url = process.env.DB_URL;
 export const connectDB = async () => {
-        await mongoose.connect('mongodb://localhost:27017/food-delivery-app')
+        await mongoose.connect(db_url)
 
                 .then(() => console.log("database connection successful"))
 }
